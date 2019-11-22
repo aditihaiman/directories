ee8163e91b043aa249060e4dbebd9f2667009f5d
aditihaiman <ahaiman00@stuy.edu>
2019-11-22 13:47:14 -0500

    added errno

--- directories.c
+++ directories.c
@@ -20,35 +20,39 @@ int main( int argc, char *argv[]){
     else path = "./";
     
     DIR * fd = opendir(path);
-    //printf("errno: %d, error: %s\n", errno, strerror(errno));
+        
+    if (errno != 0) printf("errno: %d, error: %s\n", errno, strerror(errno));
     
-    struct dirent * file = readdir(fd);
-    
-    struct stat buf;
-    
-    int size;
-    printf("Statistics for directory: %s\n", path);
-    
-    while(file != NULL) {
-        stat(file->d_name, &buf);
-        printf("Directories:\n");
-        while(file != NULL && S_ISDIR(buf.st_mode)){
-            printf("    %s\n", file->d_name);
-            file = readdir(fd);
+    else {
+        
+        struct dirent * file = readdir(fd);
+        
+        struct stat buf;
+        
+        int size;
+        printf("Statistics for directory: %s\n", path);
+        
+        while(file != NULL) {
             stat(file->d_name, &buf);
-            size += buf.st_size;
-        }
-        if (file != NULL) printf("Regular files:\n");
-        while(file != NULL && S_ISREG(buf.st_mode)) {
-            printf("    %s\n", file->d_name);
-            file = readdir(fd);
-            stat(file->d_name, &buf);
-            size += buf.st_size;
+            printf("Directories:\n");
+            while(file != NULL && S_ISDIR(buf.st_mode)){
+                printf("    %s\n", file->d_name);
+                file = readdir(fd);
+                stat(file->d_name, &buf);
+                size += buf.st_size;
+            }
+            if (file != NULL) printf("Regular files:\n");
+            while(file != NULL && S_ISREG(buf.st_mode)) {
+                printf("    %s\n", file->d_name);
+                file = readdir(fd);
+                stat(file->d_name, &buf);
+                size += buf.st_size;
 
+            }
         }
+        
+        printf("Total directory size: %d bytes\n", size);
     }
-    
-    printf("Total directory size: %d bytes\n", size);
 
     return 0;
 }
